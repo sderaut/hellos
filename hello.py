be08e72f50a2566740e1e5d15c8dce1a09461f06
sonny <sonnymoonie@gmail.com>
2015-08-03 15:59:25 -0700

    a command line Python 3 hello for Linux, with docstring

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+a Python 3 Hello World program, with a line for running as a script in Linux
+ 
+    to run from command line in Linux, supposing this file is named "hello.py"
+    $ ./hello.py
+    when in the same directory as this file, or
+    $ hellos/hello.py
+    from just outside a directory "hellos" that contains this file, and so on
+'''
+print("Hello, world!")

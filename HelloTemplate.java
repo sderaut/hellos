489b2e9607ec10b57b78ea9b6a31d4e75c37f1a6
sonny <sonnymoonie@gmail.com>
2015-08-03 17:26:02 -0700

    command line Hello from Java

---
+++ HelloTemplate.java
@@ -0,0 +1,22 @@
+// package hellos;
+
+/**
+ * This is the Javadoc for a command line Hello World program in Java.
+ * @author sde
+ */
+public class HelloTemplate {
+
+    /**
+     * @param args does little with the command line arguments
+     */
+    public static void main(String[] args) {
+        for (String arg : args) {
+            if (arg.equals("-h")) {
+                System.out.println("This program provides little help yet.");
+            }
+        }
+        
+        System.out.println("\"Hello\" from Java.");
+        
+    }
+}
